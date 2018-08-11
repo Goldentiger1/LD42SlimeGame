825f61cb16c6ce2edf47dc6d2488da7dbfd516c8
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-08-11 17:22:03 +0300

    Slime movement Done

---
+++ Slimer/Assets/TilemapMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilemapMovement : MonoBehaviour {
+
+    private GameObject player;
+    public Vector3 moveX;
+    public Vector3 moveY;
+
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown("d")) {
+            player.transform.position += moveX;
+        }
+        if (Input.GetKeyDown("a")) {
+            player.transform.position -= moveX;
+        }
+        if (Input.GetKeyDown("w")) {
+            player.transform.position += moveY;
+        }
+        if (Input.GetKeyDown("s")) {
+            player.transform.position -= moveY;
+        }
+    }
+}
