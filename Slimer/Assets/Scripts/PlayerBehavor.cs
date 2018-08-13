4cdf27bba771a8e9578bf35d8ab9c70872f35c49
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-08-13 18:45:55 +0300

    Fabric also Pickup and damage sound.

--- Slimer/Assets/Scripts/PlayerBehavor.cs
+++ Slimer/Assets/Scripts/PlayerBehavor.cs
@@ -6,12 +6,17 @@ using TMPro;
 
 public class PlayerBehavor : MonoBehaviour {
 
+    public string pickupAudio;
+    public string dmgAudio;
+
+
     GameManager gm;
     GameObject[] food;
     float foodsEaten;
     public TextMeshProUGUI FoodEaten;
     public TextMeshProUGUI statusText;
     public GameObject retryButton;
+
 
 
     void Start () {
@@ -22,21 +27,28 @@ public class PlayerBehavor : MonoBehaviour {
 
     public void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "food") {
+            //AudioManager.PlaySound("pickup");
+            Fabric.EventManager.Instance.PostEvent(pickupAudio);
             print("osuma ruokaan, kasvoit hieman");
             gm.megaSlime += 5;
             foodsEaten++;
             FoodEaten.text = "Food eaten: " + foodsEaten;
             Destroy(collision.gameObject);
+            
+
 
         }
 
         if (collision.tag == "enemy") {
+            //AudioManager.PlaySound("damage");
+            Fabric.EventManager.Instance.PostEvent(dmgAudio);
             gm.megaSlime -= 5;
             //vähennä myös scorea (ja kerrointa?)
             if (gm.megaSlime < 0) {
                 gameObject.SetActive(false);
                 statusText.text = "Gameover, better luck next time!";
                 retryButton.SetActive(true);
+                
             }
         }
 
